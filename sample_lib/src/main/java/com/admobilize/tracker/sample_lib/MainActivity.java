13fb1ff2ea38fa9101e31b0814fe6167f5294ccb
Antonio Vanegas <hpsaturn@gmail.com>
2018-12-19 23:07:50 +0100

    implemented basic UI with library schema

--- sample_lib/src/main/java/com/admobilize/tracker/sample_lib/MainActivity.java
+++ sample_lib/src/main/java/com/admobilize/tracker/sample_lib/MainActivity.java
@@ -3,6 +3,9 @@ package com.admobilize.tracker.sample_lib;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import admobilize.adtrackerlib.control.ConfigManager;
 import admobilize.adtrackerlib.control.OnDetectorActionListener;
@@ -12,11 +15,21 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
 
     private ConfigManager configManager;
+    private Button btnStartStopService;
+    private TextView tvDeviceId;
+    private TextView tvStatus;
+    private TextView tvWebhookData;
+    private boolean toggle=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStartStopService = findViewById(R.id.btn_start_stop);
+        tvDeviceId          = findViewById(R.id.tv_deviceId);
+        tvStatus            = findViewById(R.id.tv_status);
+        tvWebhookData       = findViewById(R.id.tv_webhook_data);
 
         OnDetectorActionListener onDetectorActionListener = new OnDetectorActionListener() {
 
@@ -41,6 +54,10 @@ public class MainActivity extends AppCompatActivity {
             public void onServiceStart() {
 
                 Log.d(TAG,"onServiceStart");
+                btnStartStopService.setText("STOP BACKGROUND SERVICE");
+                toggle=false;
+                btnStartStopService.setEnabled(true);
+                tvStatus.setText("");
 
             }
 
@@ -48,6 +65,10 @@ public class MainActivity extends AppCompatActivity {
             public void onServiceStop(String msg) {
 
                 Log.d(TAG,"onServiceStop: "+msg);
+                btnStartStopService.setText("START BACKGROUND SERVICE");
+                toggle=true;
+                btnStartStopService.setEnabled(true);
+                tvStatus.setText("");
 
             }
 
@@ -65,17 +86,28 @@ public class MainActivity extends AppCompatActivity {
             public void onDetectorId(String detectorId) {
 
                 Log.d(TAG,"onDetectorId: "+detectorId);
-                configManager.setServiceStart();
+                tvDeviceId.setText(detectorId);
 
             }
 
             @Override
             public void onDetectorData(String data) {
-
+                if(data!=null){
+                    Log.d(TAG,"display webhook..");
+                    int length = data.length();
+                    tvWebhookData.setText(StringUtils.formatString(data, 0, length));
+                }
             }
         };
 
         configManager = new ConfigManager(this, onDetectorActionListener);
+
+        btnStartStopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                configManager.setServiceEnable(toggle);
+            }
+        });
 
     }
 
